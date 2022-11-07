a5ab2300430c7330e522a4ad110b77d20274b41b
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-11-07 11:49:33 +0300

    fix Stats query

--- stats-server/src/main/java/ru/practicum/stats/repository/HitRepository.java
+++ stats-server/src/main/java/ru/practicum/stats/repository/HitRepository.java
@@ -11,11 +11,13 @@ import java.util.List;
 public interface HitRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("select app, uri, count (ip) as hits from EndpointHit " +
-            "where uri in :uris and (created >= :start and created <= :end)")
+            "where uri in :uris and (created >= :start and created <= :end)" +
+            "group by uri, app")
     List<HitDto> getHits(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("select app, uri, count (distinct ip) as hits from EndpointHit " +
-            "where uri in :uris and (created >= :start and created <= :end)")
+            "where uri in :uris and (created >= :start and created <= :end)" +
+            "group by uri, app")
     List<HitDto> getUniqueHits(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
