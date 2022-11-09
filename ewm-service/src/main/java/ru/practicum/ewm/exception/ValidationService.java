46b2491aed19e09b5f6d9d5f43b72b00453994f6
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-11-10 00:53:27 +0300

    fix ValidationService

--- ewm-service/src/main/java/ru/practicum/ewm/exception/ValidationService.java
+++ ewm-service/src/main/java/ru/practicum/ewm/exception/ValidationService.java
@@ -1,6 +1,7 @@
 package ru.practicum.ewm.exception;
 
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import ru.practicum.ewm.category.dto.CategoryDto;
 import ru.practicum.ewm.compilation.dto.CompilationDtoInput;
 import ru.practicum.ewm.user.dto.UserDto;
@@ -8,6 +9,7 @@ import ru.practicum.ewm.user.dto.UserDto;
 import javax.validation.Valid;
 
 @Component
+@Validated
 public class ValidationService {
     public UserDto validateUser(@Valid UserDto userDto) {
         return userDto;
